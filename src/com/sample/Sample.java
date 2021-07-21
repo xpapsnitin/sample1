31b272e49420ff0e3e4d6014ad91ad245262c93f
Owner <nitinpatilat@gmail.com>
2021-07-21 18:40:43 +0530

    next message

--- src/com/sample/Sample.java
+++ src/com/sample/Sample.java
@@ -12,7 +12,7 @@ public class Sample {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) {	
 		// TODO Auto-generated method stub
 
 	}
